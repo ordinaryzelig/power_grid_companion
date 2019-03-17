2414496106074cf05865a3ac0923bfaec8d36f92
Jared Ning <jared@redningja.com>
2019-03-16 19:45:02 -0500

    Add turn order track. Start SASS organization for players.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
 
+private
+
   def current_player
-    @current_player ||= Player.find(cookies.signed[:player_id])
+    #@current_player ||= Player.find(cookies.signed[:player_id])
+    @current_player ||= Game.find(1).players.first
   end
+  helper_method :current_player
 
   def current_game
     @current_game ||= current_player.game
   end
+  helper_method :current_game
 
 end
