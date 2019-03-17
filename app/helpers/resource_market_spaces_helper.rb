1d3dd194297f518c26a132cc7fbc98bff099583f
Jared Ning <jared@redningja.com>
2019-03-17 09:32:50 -0500

    Refactor resource market space with mixin. Trash color.

--- app/helpers/resource_market_spaces_helper.rb
+++ app/helpers/resource_market_spaces_helper.rb
@@ -3,8 +3,8 @@ module ResourceMarketSpacesHelper
   def resource_market_space_box(space)
     options = {}
     options[:class] = ['resource_market_space']
-    options[:class] << space.kind
     options[:class] << 'occupied' if space.resource.present?
+    options[:data] = {:kind => space.kind}
     content_tag :div, options do
       resource_icon(space.kind)
     end
