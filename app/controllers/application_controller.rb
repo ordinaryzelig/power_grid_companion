0d62a263e659d65ea005b1e155c341d9080ca3a3
Jared Ning <jared@redningja.com>
2019-03-30 18:23:29 -0500

    Allow current_player/game to be blank.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
 private
 
   def current_player
-    @current_player ||= Player.find(cookies.signed[:player_id])
+    @current_player ||= Player.find_by(:id => cookies.signed[:player_id]) if cookies.signed[:player_id]
   end
   helper_method :current_player
 
   def current_game
-    current_player.game
+    current_player.game if current_player
   end
   helper_method :current_game
 
